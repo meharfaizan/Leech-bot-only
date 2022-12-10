3d47947c38fda2f539035b5f11080d9032d2eced
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-10 20:07:06 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo...."
 git clone https://github.com/meharfaizan/Leech-bot-only
 cd /Leech-bot-only
-apt -qq install -y git aria2
+apt -qq update --fix-missing && apt -qq install -y git aria2 wget curl busybox unzip unrar tar python3 ffmpeg python3-pip p7zip-full p7zip-rar
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 -m tobrot
