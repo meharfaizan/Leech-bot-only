0a3540d5c8edcb9cbdb8c3e640e785262c0044c1
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-10 18:04:23 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo...."
 git clone https://github.com/meharfaizan/Leech-bot-only
 cd /Leech-bot-only
-pip3 install Dockerfile
+apt -qq install -y git aria2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 -m tobrot
