b6a5a0dff53c01be9fdddf8886f971f992396a91
meharfaizan <87762660+meharfaizan@users.noreply.github.com>
2022-12-11 16:38:52 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/meharfaizan/Leech-bot-only
-git clone https://github.com/aria2/aria2
+git clone https://github.com/aria2/aria2.git
 cd /Leech-bot-only
 apt -qq update --fix-missing && apt -qq install -y git aria2 wget curl busybox unzip unrar tar python3 ffmpeg python3-pip p7zip-full p7zip-rar
 pip3 install -r requirements.txt
